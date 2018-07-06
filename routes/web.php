a5853c1d7f3d8b3b684be7d5e8a1c0d3027488f3
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-06 09:57:13 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -278,7 +278,7 @@ Route::get('/vpn_auth', function (Request $request) {
 
 Route::get('/sample', function () {
     $server = Server::findorfail(5);
-    $users = $server->online_users();
+    $users = $server->online_users;
     return $users;
 });
 
