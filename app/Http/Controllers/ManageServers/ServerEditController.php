07630c3f5dc56b1e98484f75b0c9e9058a775271
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-10 19:10:18 +0800

    Update

--- app/Http/Controllers/ManageServers/ServerEditController.php
+++ app/Http/Controllers/ManageServers/ServerEditController.php
@@ -90,7 +90,7 @@ class ServerEditController extends Controller
     {
         $server= Server::findorfail($id);
 
-        $users = User::whereIn('id', json_decode($server->privateUsers->pluck('user_id')));
+        $users = User::whereIn('id', json_decode($server->privateUsers->pluck('id')));
 
         return datatables()->eloquent($users)
             ->addColumn('check', '<input type="hidden" class="user_id" value="{{ $id }}">')
