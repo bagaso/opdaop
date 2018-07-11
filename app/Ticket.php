57b2f650bfc1271616102372856c86bc60b888f1
mp3sniff <mp3sniff@gmail.com>
2018-07-11 20:36:51 +0800

    Update

--- app/Ticket.php
+++ app/Ticket.php
@@ -57,10 +57,10 @@ class Ticket extends Model
     {
         return $query->where(function ($query) {
             if(auth()->user()->cannot('MANAGE_SUPPORT')) {
-                $query->where('user_id', auth()->user()->id)
+                $query->where('user_id', auth()->user()->id);
             }
         })->where(function ($query) {
-            if(auth()->user()->group_id > $this->user_by->group_id) {
+            if(auth()->user()->group_id > $query->user_by->group_id) {
                 return true;
             }
             return false;
