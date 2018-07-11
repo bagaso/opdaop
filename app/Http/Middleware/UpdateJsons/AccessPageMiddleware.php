1ced0f32d1c2d1c7dc7b2ec76329ceefe7ae673d
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-11 16:46:16 +0800

    Update

--- app/Http/Middleware/UpdateJsons/AccessPageMiddleware.php
+++ app/Http/Middleware/UpdateJsons/AccessPageMiddleware.php
@@ -17,6 +17,9 @@ class AccessPageMiddleware
     public function handle($request, Closure $next)
     {
         if(auth()->user()->isActive()) {
+            if(auth()->user()->isAdmin()) {
+                return $next($request);
+            }
             if(in_array(auth()->user()->group->id, [2])) {
                 return $next($request);
             }
