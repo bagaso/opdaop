98fa595448e6e6b3c0f2af18db217843fb1e20fb
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-13 16:55:11 +0800

    Update

--- app/Rules/Settings/DataAllowanceFormatRule.php
+++ app/Rules/Settings/DataAllowanceFormatRule.php
@@ -25,7 +25,7 @@ class DataAllowanceFormatRule implements Rule
      */
     public function passes($attribute, $value)
     {
-        $data_allowance_type = substr($value, -1, 2);
+        $data_allowance_type = substr($value, -2, 2);
         if(in_array($data_allowance_type, ['mb', 'gb'])) {
             return  true;
         }
