8dd877efe0b2606ba008f9d4062246aa0fe2cc16
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-16 00:08:05 +0800

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -69,6 +69,7 @@ class AppServiceProvider extends ServiceProvider
                     Config::set('backup.backup.gzip_database_dump', app('settings')->backup_gzip_database_dump ? app('settings')->backup_gzip_database_dump : '');
                     Config::set('backup.backup.destination.filename_prefix', app('settings')->backup_filename_prefix ? app('settings')->backup_filename_prefix : '');
                     Config::set('backup.backup.destination.disks', app('settings')->backup_disks ? app('settings')->backup_disks : '');
+                    Config::set('backup.backup.notifications.mail.to', app('settings')->site_email ? app('settings')->site_email : '');
                     $monitor[] = [
                         'name' => app('settings')->backup_directory,
                         'disks' => [
