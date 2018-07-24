dced69d771b0d6a424af9e05d81591ab3e5d0193
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-24 13:45:40 +0800

    Update

--- resources/views/theme/default/layouts/panel.blade.php
+++ resources/views/theme/default/layouts/panel.blade.php
@@ -44,7 +44,7 @@
     <!-- class="main-header" -->
     <header class="main-header">
         <!-- Logo -->
-        <a href="../../index2.html" class="logo">
+        <a href="{{ app('settings')->site_url }}" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>VPN</b></span>
             <!-- logo for regular state and mobile devices -->
