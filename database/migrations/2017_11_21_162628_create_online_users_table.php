b179b09759d10eca89fab4399d3881d8cc022742
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-25 11:51:49 +0800

    Update

--- database/migrations/2017_11_21_162628_create_online_users_table.php
+++ database/migrations/2017_11_21_162628_create_online_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -14,7 +15,7 @@ class CreateOnlineUsersTable extends Migration
     public function up()
     {
         Schema::create('online_users', function (Blueprint $table) {
-            $table->uuid('id')->unique()->default(`uuid()`);
+            $table->uuid('id')->unique()->default(DB::raw('uuid()'));
             $table->integer('user_id')->index();
             $table->string('protocol');
             $table->integer('server_id')->index();
