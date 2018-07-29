03fad0a21d87950229a71f126df7db5896e2e053
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-07-29 17:55:29 +0800

    Update

--- app/Http/Controllers/ManageUsers/UserProfileController.php
+++ app/Http/Controllers/ManageUsers/UserProfileController.php
@@ -52,6 +52,7 @@ class UserProfileController extends Controller
         $expired_at = Carbon::parse($user->getOriginal('expired_at'));
 
         $user_subscription = Subscription::findorfail($request->subscription);
+        $old_user_subscription = Subscription::findorfail($user->subscription_id);
 
         $user->group_id = $request->group ? $request->group : $user->group_id;
         $user->username = $request->username ? strtolower($request->username) : strtolower($user->username);
@@ -62,7 +63,7 @@ class UserProfileController extends Controller
         $user->max_users = $request->max_users ? $request->max_users : $user->max_users;
         $user->status_id = $request->status;
         $user->subscription_id = $request->subscription ? $request->subscription : $user->subscription_id;
-        $user->expired_at = !$user->paidSubscription() ? ($user->subscription_id <> $user_subscription->id ? $date_now->copy()->addSeconds((($date_now->diffInSeconds($expired_at) * intval($user->subscription->cost)) / intval($user_subscription->cost))) : $expired_at) : $expired_at;
+        $user->expired_at = $user->paidSubscription() ? ($old_user_subscription->id <> $user_subscription->id ? $date_now->copy()->addSeconds((($date_now->diffInSeconds($expired_at) * intval($old_user_subscription->cost)) / intval($user_subscription->cost))) : $expired_at) : $expired_at;
         $user->save();
         if($old_group_id <> ($request->group ? $request->group : $old_group_id)) {
             $permissions = Permission::where([['group_id', ($request->group ? $request->group : $user->group_id)], ['is_default', 1]])->pluck('id');
