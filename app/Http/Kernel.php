95605d8022dbd4f08585a34bb3d33aa48b7e6250
mp3sniff@gmail.com <mp3sniff@gmail.com>
2018-10-01 11:03:52 +0800

    Update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckUserIfSuspendedMiddleware::class,
+            'throttle:30,1',
         ],
 
         'api' => [
