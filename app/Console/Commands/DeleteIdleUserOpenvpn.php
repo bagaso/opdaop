e15aa52b4aa5d2f8a560f326348f127e15526e2d
bagaso <mp3sniff@gmail.com>
2019-03-04 18:17:20 +0800

    Update

--- app/Console/Commands/DeleteIdleUserOpenvpn.php
+++ app/Console/Commands/DeleteIdleUserOpenvpn.php
@@ -2,7 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\OnlineUser;
+use App\User;
 use Illuminate\Console\Command;
+
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class DeleteIdleUserOpenvpn extends Command
 {
@@ -37,6 +43,46 @@ class DeleteIdleUserOpenvpn extends Command
      */
     public function handle()
     {
-        //
+        try {
+            DB::connection()->getPdo();
+            if(Schema::hasTable('settings')) {
+                $delete_idle = OnlineUser::with(['user', 'server'])->where('updated_at', '<=', \Carbon\Carbon::now()->subMinutes(3));
+
+                foreach ($delete_idle->get() as $online_user) {
+                    try {
+                        $user = User::findorfail($online_user->user_id);
+                        $job = (new JobVpnDisconnectUser($user->username, $online_user->server->server_ip, $online_user->server->server_port))->onConnection(app('settings')->queue_driver)->onQueue('disconnect_user');
+                        dispatch($job);
+                        $user->timestamps = false;
+                        if(!$user->isAdmin() && $online_user->server->limit_bandwidth && $online_user->data_available > 0) {
+                            $data = doubleval($online_user->data_available) - doubleval($online_user->byte_sent);
+                            $user->consumable_data = ($data >= 0) ? $data : 0;
+                            $user->save();
+                        }
+                        $user->lifetime_bandwidth = doubleval($user->lifetime_bandwidth) + doubleval($online_user->byte_sent);
+                        $user->save();
+
+                        $vpn_history = new HistoryVpn;
+                        $vpn_history->user_id = $online_user->user_id;
+                        $vpn_history->protocol = $online_user->protocol;
+                        $vpn_history->user_ip = $online_user->user_ip;
+                        $vpn_history->user_port = $online_user->user_port;
+                        $vpn_history->server_name = $online_user->server->server_name;
+                        $vpn_history->server_ip = $online_user->server->server_ip;
+                        $vpn_history->sub_domain = $online_user->server->sub_domain;
+                        $vpn_history->byte_sent = floatval($online_user->bytes_sent);
+                        $vpn_history->byte_received = floatval($online_user->bytes_received);
+                        $vpn_history->session_start = Carbon::parse($online_user->getOriginal('created_at'));
+                        $vpn_history->session_end = Carbon::now();
+                        $vpn_history->save();
+                    } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $ex) {
+                        //
+                    }
+                }
+                $delete_idle->delete();
+            }
+        } catch (Exception $e) {
+            //die("Could not connect to the database.  Please check your configuration.");
+        }
     }
 }
